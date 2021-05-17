d1a024513376b94352974c3b92e9258009c916ed
learninglegion <brent.beckwith@gmail.com>
2021-05-17 16:00:37 -0600

    first commit

---
+++ Cryptotracker.py
@@ -0,0 +1,24 @@
+# Program to record purchases of crypto. X dollars @ Y price results in
+# Z coin ownership. E.g. $200 when BTC is at 4371.08 = .00164 BTC
+# Date of purchase optional.
+
+# Secondary function will pull current crypto value and calculate
+# percentage increase along with absolute value increase
+
+print("Let's track your crypto purchases!")
+print("Enter 'q' at any time to quit")
+
+# Ask for dollars spent in purchase
+print("First, how much money did you spend on this purchase?")
+input("Dollar amount, including cents: ")
+# Record dollars spent in purchase
+
+# Ask for price of crypto at time of purchase
+# Record price of crypto at time of purchase
+
+# Ask for amount of crypto obtained (after fees)
+# Record amount of crypto obtained (after fees)
+
+# Ask for date purchased
+# Record date purchased
+
