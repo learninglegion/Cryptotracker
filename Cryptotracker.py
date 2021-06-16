eae4062ca21e50e42d0f47feaa8c8220d22b8681
learninglegion <brent.beckwith@gmail.com>
2021-06-15 19:45:45 -0600

    Refactored to functions

--- Cryptotracker.py
+++ Cryptotracker.py
@@ -1,6 +1,6 @@
 # Program to record purchases of crypto. X dollars @ Y price results in
-# Z coin ownership. E.g. $3500 when BTC is at 35000 = .1 BTC
-# Date of purchase optional.
+# Z coin ownership. E.g. $3500 of BTC at 35000 = .1 BTC
+# Date of purchase optional. Variables given not calculated currently.
 
 # Options in program will be to enter new purchases and review/modify/
 # delete previously entered purchases
@@ -10,17 +10,18 @@
 # entry
 
 # Stretch goals include multiple choice crypto selection in
-# order to use proper API to pull crypto value.
+# order to use proper API to pull crypto value as well as an
+# api (coinbase?) to allow people to purchase crypto in app.
+# Different currencies also possible.
 
-# example currency formats - requires float to convert to string
-# print("{:.2f}".format(ledger_entry.get('Fiat amount purchased')))
-# print("{:.2f}".format(buy_dollar))
+
+#######################################################################
 
 # Initialize dictionary to record info
 # Maybe change this process
 ledger_entry= {}
 
-#Next step, make entry a class?
+# Next refactoring, make purchase a class - instantiate for each entry
 
 #Initial test questions below
 
@@ -31,28 +32,29 @@ def intro():
 intro()
 
 #Questions for variable generation - conjure better variables
-p_dollars = input("First, how much money did you spend on this crypto purchase? ")
-formatted_dollars = float(p_dollars)
-c_name = input("What was the name of the crypto you bought? ")
-o_c_price = input(f"How much was -ONE- {c_name} when bought? ")
-c_obtained = input(f"How much {c_name} did you own after fees? ")
+# order_dollars = input("First, dollar amount spent on this crypto purchase? ")
+# f_order_dollars = float(order_dollars)
+# order_crypto = input("What was the name of the crypto you bought? ")
+# order_price = input(f"How much was -ONE- {order_crypto} when bought? ")
+# order_bought = input(f"How much {order_crypto} did you own after fees? ")
 
 # Ask for dollars spent in purchase (force money format,
 # only numbers and 1 required decimal, limit to 14 characters (billions
 # plus cents)
 
-#print("First, how much money did you spend on this crypto purchase?")
-#buy_dollar = Decimal(input("Dollar amount, including cents: "))
-
+# example currency formats - requires float to convert to string
+# print("{:.2f}".format(ledger_entry.get('Fiat amount purchased')))
+# print("{:.2f}".format(buy_dollar))
 #testing
-# buy_dollar = 3500.00
-def money_spent(buy_dollar):
-    # print("First, how much money did you spend on this crypto purchase?")
-    # buy_dollar = 3500.00
-    print("${:.2f}".format(buy_dollar)) # string output
-    ledger_entry['buy_dollar'] = buy_dollar
+# order_dollars = 3500.00
+def record_money_spent():
+    order_dollars = input("First, dollar amount spent on this crypto purchase?\n")
+    # dollars = 3500.00
+    # print("${:.2f}".format(order_dollars)) # string output
+    f_order_dollars = float(order_dollars)
+    ledger_entry['dollars'] = f_order_dollars
 
-money_spent(formatted_dollars)
+record_money_spent()
 
 # Ask what crypto was bought (push string format for now, change to
 # multiple choice eventually to minimize errors when matching log
@@ -62,14 +64,14 @@ money_spent(formatted_dollars)
 #bought? ")
 
 #testing
-# buy_crypto_name = "BTC"
-def crypto_name_bought(buy_crypto_name):
-    print("What was the name of the crypto you bought?")
-    # buy_crypto_name = "BTC"
-    print(buy_crypto_name)
-    ledger_entry['buy_crypto_name'] = buy_crypto_name
+# crypto_name = "BTC"
+def record_crypto_name():
+    order_crypto = input("What was the name of the crypto you bought?\n")
+    # order_crypto = "BTC"
+    print(order_crypto)
+    ledger_entry['order_crypto'] = order_crypto
 
-crypto_name_bought(c_name)
+record_crypto_name()
 
 # Ask for price of crypto at time of purchase (numbers only, 1 decimal
 # required (pre-populate in web interface?), max 20 characters)
@@ -78,13 +80,13 @@ crypto_name_bought(c_name)
 # {buy_crypto_name} worth when you bought it? ")
 
 #testing
-def crypto_purchase_price(buy_crypto_price, cnb = c_name):
-    print(f"How much was -ONE- {cnb} when bought?")
+def record_crypto_price(name = ledger_entry['order_crypto']):
+    order_price = input(f"How much was -ONE- {name} when bought? ")
     # buy_crypto_price = 35000
-    print(buy_crypto_price)
-    ledger_entry['buy_crypto_price'] = buy_crypto_price
+    print(order_price)
+    ledger_entry['order_price'] = order_price
 
-crypto_purchase_price(o_c_price)
+record_crypto_price()
 
 # Record/format price of crypto at time of purchase
 
@@ -95,13 +97,13 @@ crypto_purchase_price(o_c_price)
 # you own after fees? ")
 
 #testing
-def crypto_obtained(purchase_crypto_obtained, cnb = c_name):
-    print(f"How much {cnb} did you own after fees?")
+def record_crypto_obtained(name = ledger_entry['order_crypto']):
+    order_bought = input(f"How much {name} did you own after fees? ")
     # purchase_crypto_obtained = .1
-    print(purchase_crypto_obtained)
-    ledger_entry['purchase_crypto_obtained'] = purchase_crypto_obtained
+    print(order_bought)
+    ledger_entry['order_bought'] = order_bought
 
-crypto_obtained(c_obtained)
+record_crypto_obtained()
 
 # Record/format amount of crypto obtained (after fees)
 
@@ -120,11 +122,12 @@ crypto_obtained(c_obtained)
 # Verify entry, push proper format - if confirmation response is 'no'
 # then go back to beginning of loop or ask for element to correct.
 
-print(f"Let's make sure this is all correct. You spent \
-{'${:.2f}'.format(formatted_dollars)} dollars on {c_name} when one \
-{c_name} was worth ${o_c_price} and \
-own {c_obtained} {c_name} as a \
-result of this transaction?")
+print(f"Let's make sure this is all correct. You spent\
+ {'${:.2f}'.format(ledger_entry['dollars'])} dollars on\
+ {ledger_entry['order_crypto']} when one {ledger_entry['order_crypto']}\
+ was worth ${ledger_entry['order_price']} and own\
+ {ledger_entry['order_bought']} {ledger_entry['order_crypto']} as a\
+ result of this transaction?")
 
     
 # p_dollars = input("First, how much money did you spend on this crypto purchase?")
@@ -135,4 +138,4 @@ result of this transaction?")
 #After confirmation, record/format values
 # Use dictionary of dictionaries with entry number as key?
 # Use database and store records?
-print(ledger_entry)
+# print(ledger_entry)
